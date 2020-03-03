78016320e178052ccaa4562e3619b986dd3aa142
Josh Montoya <josh@montoya.io>
2020-03-03 11:19:03 -0800

    Implement examples

--- configuration_test.go
+++ configuration_test.go
@@ -26,3 +26,25 @@ func TestConfiguration_Printf(t *testing.T) {
 	c := New(Bold, FGGreen)
 	c.Printf("Hello world! My name is %s %s\n", "John", "Doe")
 }
+
+func ExampleConfiguration_Sprint() {
+	c := New(Bold, FGGreen)
+	out := c.Sprint("Hello world!\n")
+	fmt.Printf("Sprint: %s", out)
+}
+
+func ExampleConfiguration_Sprintf() {
+	c := New(Bold, FGGreen)
+	out := c.Sprintf("Hello world! My name is %s %s\n", "John", "Doe")
+	fmt.Printf("Sprintf: %s", out)
+}
+
+func ExampleConfiguration_Print() {
+	c := New(Bold, FGGreen)
+	c.Print("Hello world!\n")
+}
+
+func ExampleConfiguration_Printf() {
+	c := New(Bold, FGGreen)
+	c.Printf("Hello world! My name is %s %s\n", "John", "Doe")
+}
