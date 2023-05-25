c0b7b7c5da97c0e05171e22db1e1a6314ce3c09e
itsMontoya <josh@montoya.io>
2023-05-25 13:44:44 -0700

    add formatting abilities to Attribute

--- attribute.go
+++ attribute.go
@@ -1,5 +1,10 @@
 package colors
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Styling attributes
 const (
 	Reset Attribute = iota
@@ -64,3 +69,36 @@ const (
 
 // Attribute defines a single SGR Code
 type Attribute int
+
+func (a Attribute) String() string {
+	return strconv.Itoa(int(a))
+}
+
+// Sprint will return a colored version of the value
+func (a Attribute) Sprint(value string) (out string) {
+	return fmt.Sprintf(colorFormat, a.String(), value, Reset)
+}
+
+// Sprintf will return a colored version for the format and values
+func (a Attribute) Sprintf(format string, values ...interface{}) (out string) {
+	value := fmt.Sprintf(format, values...)
+	return a.Sprint(value)
+}
+
+// Print will output colored version of a given string
+func (a Attribute) Print(value string) {
+	out := a.Sprint(value)
+	fmt.Print(out)
+}
+
+// Printf will output colored version of a given format and values
+func (a Attribute) Printf(format string, values ...interface{}) {
+	out := a.Sprintf(format, values...)
+	fmt.Print(out)
+}
+
+// Printf will output colored version of a given format and values
+func (a Attribute) Println(format string, values ...interface{}) {
+	out := a.Sprintf(format, values...)
+	fmt.Println(out)
+}
